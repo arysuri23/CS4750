031f9b62cafc147c83547e8f823f8114f4603c5b
arysuri <59773116+arysuri23@users.noreply.github.com>
2023-04-06 13:40:24 -0400

    Update restaurants.php

--- restaurants.php
+++ restaurants.php
@@ -1,7 +1,58 @@
+
 <?php 
-    require 'config.php';
-
-    echo 'Restaurant List';
-
+    require "config.php";
+    if(!isset($_SESSION['login_id'])){
+        header('Location: login.php');
+        exit;
+    }
+    $id = $_SESSION['login_id'];
+    $get_google_user = mysqli_query($db_connection, "SELECT * FROM `google_users` WHERE `google_id`='$id'");
+    if(mysqli_num_rows($get_google_user) > 0){
+        $user = mysqli_fetch_assoc($get_google_user);
+    }
+    else{
+        header('Location: logout.php');
+        exit;
+    }
+    
+    $email = $user['email'];
+    $restaurants = mysqli_query($db_connection, "SELECT * FROM `restaurant`;");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>F</title>
+</head>
+
+<body>
+    <div class="row justify-content-center"> 
+    
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:50%">
+      <thead>
+      <tr style="background-color:#B0B0B0">
+        <th width="20%">Name        
+        <th width="20%">Address        
+        <th width="5%">Open 
+          <th width="5%">Close
+      </tr>
+      </thead>
+    <?php foreach ($restaurants as $row): ?>
+      <tr>
+         <td><?php echo $row['name']; ?></td>
+         <td><?php echo $row['address']; ?></td>
+         <td><?php echo $row['open']; ?></td>        
+         <td><?php echo $row['close']; ?></td>  
+      </tr>
+    <?php endforeach; ?>
+    </table>
+    </div>
+</body>
+
+</html>
