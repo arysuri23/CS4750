e4b1b19ec87081ef4dbff32362e254df80002830
Miles Woollacott <officialmwoollacott@gmail.com>
2023-04-05 21:31:36 -0400

    Started integrating DB

--- config.php
+++ config.php
@@ -2,11 +2,30 @@
 session_start();
 session_regenerate_id(true);
 // change the information according to your database
-$db_connection = mysqli_connect("localhost:3306","root","","restaurant_review");
-// CHECK DATABASE CONNECTION
-if(mysqli_connect_errno()){
-    echo "Connection Failed".mysqli_connect_error();
-    exit;
+/** S23, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on local XAMPP) **/
+$username = 'nkoix';
+$password = 'Cesttresf1n@le';
+$host = 'localhost:3307';           // default phpMyAdmin port = 3306
+$dbname = 'restaurant_review';
+$dsn = "mysql:host=$host;dbname=$dbname";  
+////////////////////////////////////////////
+
+try
+{
+    $db_connection = new PDO($dsn, $username, $password);
+    echo "Success";
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
 }
 
 
--- restaurants.php
+++ restaurants.php
@@ -1,7 +1,29 @@
 <?php 
-    require 'config.php';
+    require("config.php");
 
-    echo 'Restaurant List';
+    function getRestaurants()
+    {
+      global $db_connection;
+      $query = "select * from restaurants";
+      $statement = $db_connection->prepare($query);
+      $statement->execute();
+      $result = $statement->fetchAll();
+      $statement->closeCursor();
+      return $result;
+    }
 
-
+    $restaurants = getRestaurants();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>F</title>
+</head>
+
+<body>
+    <h1>Restaurants</h1>
+</body>
+
+</html>
